2e9ef6e87e06acc65ba6789243cedc0c99d183c8
Vizvul <113857968+Vizvul@users.noreply.github.com>
2022-10-14 15:46:51 +0300

    Lesson 1 HomeWork Done

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import com.sun.source.tree.IfTree;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите первое число: ");
+        int a = sc.nextInt();
+        System.out.println("ВВедите второе число: ");
+        int b = sc.nextInt();
+        int result = a - b;
+        if (result < 0) {
+            System.out.println("Разница двух чисел меньше нуля и равна: " + result);
+        } else if (result > 0) {
+            System.out.println("Разница двух чисел больше нуля и равна: " + result);
+        } else {
+            System.out.println("Разница двух чисел равна нулю");
+        }
+    }
+}
